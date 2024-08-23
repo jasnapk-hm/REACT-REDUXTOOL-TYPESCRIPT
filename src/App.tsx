c3442026c1f2ee07a441acdd46223c307097a369
jasnapk-hm <hmjasnapk@gmail.com>
2024-08-23 15:58:39 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -60,10 +60,9 @@ function App() {
             This is a Dog App built with React JS Using the Dog API. The app
             uses:
             <li>ReactJS & TypeScript </li>
-            <li>Redux for State Management</li>
+            <li>Reduxtoolkit for State Management</li>
             <li>Axios for fetching Data</li>
-            <li>Styled Components</li>
-            <li> Unit Tests With Jest & React Testing Library</li>
+
           </ul>
         </Description>
 
